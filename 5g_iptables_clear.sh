d8cbe2d60974112322a6d111e9d405648435a287
Kuba <104521083+jpasierbb@users.noreply.github.com>
2024-09-02 21:32:49 +0200

    Minor changes

--- 5g_iptables_clear.sh
+++ 5g_iptables_clear.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# Definiowanie typu łancucha (chain)
+# Definiowanie chainu
 CHAIN="DOCKER-USER"
 
 # Wylistowanie regul zawierajacych "br-" w interfejsie in lub out i usuniecie ich
 iptables -S $CHAIN | grep -E ' -i br-|-o br-' | while read -r line; do
     rule=$(echo $line | sed "s/-A $CHAIN //")
-    # Usunięcie reguly z iptables
+    # Usuniecie reguly
     iptables -D $CHAIN $rule
     if [ $? -eq 0 ]; then
         echo "Regula usunieta: $rule"
